be5f2767a3ea746a00cd0c50cab54b7d45423e59
QuevDev <quevdev@outlook.com>
2023-07-05 15:32:13 -0400

    Updated routing for posts & dynamic posts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 export default function Navbar() {
   return (
-    <nav className='p-4 shadow-sm'>
+    <nav className='p-4 md:px-8 shadow-sm'>
       <ul className='flex flex-row justify-between'>
         <li>
           <a href='/'>Home</a>
         </li>
-        <li className='flex flex-row gap-2'>
+        <li className='flex flex-row gap-4'>
           <a href='/about'>About</a>
           <a href='/posts'>Posts</a>
         </li>
